e54de38992a50a43d2414bf7ae10924303a46c6c
jeanriffel <jeanriffel1@gmail.com>
2023-08-09 23:41:03 -0300

    feat: styling front-end

--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
-import { Button, TextField, Typography } from '@mui/material';
+import { Button, TextField, Typography, Table, TableBody,
+  TableCell, TableContainer, TableHead,
+  TableRow, Paper } from '@mui/material';
 import axios from 'axios';
+import index from './index.module.css'
 
 export default function Home() {
   const [betValue, setBetValue] = useState('');
@@ -10,13 +13,14 @@ export default function Home() {
   const [contractName, setContractName] = useState('');
   const [randomNumber, setRandomNumber] = useState('');
   const [winner, setWinner] = useState('');
+  const [players, setPlayers] = useState<any[]>([]);
 
   useEffect( ()=>{
     handleContractName();
   }, [])
 
   const handleWinner = async () => {
-    const instance = axios.create({ 
+/*    const instance = axios.create({ 
       baseURL: 'http://localhost:3001'
     })
 
@@ -27,10 +31,8 @@ export default function Home() {
       .catch(error => {
         console.log(error)
       });    
+*/
 
-  }
-
-  const handleBet = async () => {
 
     const payload = {
       betValue,
@@ -38,6 +40,7 @@ export default function Home() {
       address
     }
 
+    /*
     const instance = axios.create({ 
       baseURL: 'http://localhost:3001'
     })
@@ -49,6 +52,14 @@ export default function Home() {
       .catch(error => {
         console.log(error)
       });
+    
+*/
+
+  }
+
+  const handleBet = () => {
+    const player = { gambler, betValue, betAmount, address }
+    setPlayers((prevPlayers) => [...prevPlayers, player,]);
   };
 
   const handleContractName = async () =>{
@@ -66,48 +77,110 @@ export default function Home() {
       });
   }
 
+  const getContractName = () => contractName  ? contractName : 'Sorry, The contract is unvailable now!';
+
+  const getPlayers = () =>{
+    return ( 
+      <TableContainer  component={Paper}>
+        <Table sx={{ minWidth: 650, backgroundColor: 'silver' }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Gambler</TableCell>
+              <TableCell>BetValue</TableCell>
+              <TableCell>BetAmount</TableCell>
+              <TableCell>Address</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            { players.map((player) =>  
+              <TableRow>
+                <TableCell>{player.gambler}</TableCell>
+                <TableCell>{player.betValue}</TableCell>
+                <TableCell>{player.betAmount}</TableCell>
+                <TableCell>{player.address}</TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    )
+  }
+
+  const handleClearPlayers = () => {
+    setPlayers([])
+  }
+
   return (
-    <div>
+    <div  >
       <Typography
-        variant="h2" 
+        variant="h3" 
         gutterBottom
       >
-       {contractName}
+       {getContractName()}
       </Typography>
-      <div>
-        <TextField
-          variant="outlined"    
-          label="Gambler"
-          onChange={(e) => setGambler(e.target.value)}
-        />
-        <TextField
-          variant="outlined"    
-          label="Bet Value" 
-          onChange={(e) => setBetValue(e.target.value)}
-        />
-        <TextField
-          variant="outlined" 
-          label="Bet Amount"          
-          onChange={(e) => setBetAmount(e.target.value)}
-        />
-        <TextField
-          variant="outlined" 
-          label="Address"          
-          onChange={(e) => setAddress(e.target.value)}
-        />        
-        <Button 
-          variant="contained"
-          onClick={handleBet}
-        >
-          Place Bet
-        </Button>
+      <div className={index.container}>
+        
+        <div className={index.field}>
+          <TextField
+            sx={{width: '500px', padding: '10px'}}
+            variant="outlined"    
+            label="Gambler"
+            onChange={(e) => setGambler(e.target.value)}
+          />
+        </div>
+        <div className={index.field}>
+          <TextField
+            sx={{width: '500px', padding: '10px'}}
+            variant="outlined"    
+            label="Bet Value" 
+            onChange={(e) => setBetValue(e.target.value)}
+          />
+        </div>
+        <div className={index.field}>
+          <TextField
+            sx={{width: '500px', padding: '10px'}}
+            variant="outlined" 
+            label="Bet Amount"          
+            onChange={(e) => setBetAmount(e.target.value)}
+          />
+        </div >
+        <div className={index.field}>
+          <TextField
+            sx={{width: '500px', padding: '10px'}}
+            variant="outlined" 
+            label="Account Address"          
+            onChange={(e) => setAddress(e.target.value)}
+          />    
+        </div>    
 
-        <Button 
-          variant="contained"
-          onClick={handleWinner}
-        >
-          Get Winner
-        </Button>
+        <div className={index.button_container}>
+          <Button 
+            sx={{width: '248px', padding: '2px'}}
+            variant="contained"
+            onClick={handleBet}
+          >
+            Place Bet
+          </Button>
+
+          <Button 
+            sx={{width: '248px', padding: '2px'}}
+            variant="contained"
+            onClick={handleWinner}
+          >
+            Get Winner
+          </Button>
+          <Button 
+            sx={{width: '248px', padding: '2px', background: 'red'}}
+            variant="contained"
+            onClick={handleClearPlayers}
+          >
+            Clear Players
+          </Button>
+
+        </div>
+        <div className={index.table}>
+          {getPlayers()}
+        </div>
       </div>
       {randomNumber && <p>Random Number: {randomNumber}</p>}
       {winner && <p>Winner: {winner}</p>}
