4ad7be04b1a2a3248ff7692eab61f080faaeaa5b
jeanriffel <jeanriffel1@gmail.com>
2024-01-28 13:47:57 -0300

    chore: updating tabs

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -6,38 +6,38 @@ contract Lottery {
     address[] public players;
 
     constructor() {
-        manager = msg.sender;
+      manager = msg.sender;
     }
 
     function enter() public payable {
-        require(msg.value > 0.01 ether, "Minimum bet is 0.01 ether");
-        players.push(msg.sender);
+      require(msg.value > 0.01 ether, "Minimum bet is 0.01 ether");
+      players.push(msg.sender);
     }
 
     function random() private view returns (uint) {
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
+      return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players)));
     }
 
     function pickWinner() public restricted {
-        uint index = random() % players.length;
-        address winner = players[index];
-        address payable winnerPayable = payable(winner);
-        winnerPayable.transfer(address(this).balance);
+      uint index = random() % players.length;
+      address winner = players[index];
+      address payable winnerPayable = payable(winner);
+      winnerPayable.transfer(address(this).balance);
 
-        players = new address[](0);
+      players = new address[](0);
     }
 
     modifier restricted() {
-        require(msg.sender == manager, "Only the manager can call this function");
-        _;
+      require(msg.sender == manager, "Only the manager can call this function");
+      _;
     }
 
     function getPlayers() public view returns (address[] memory) {
-        return players;
+      return players;
     }
 
     function contractName() public pure returns(string memory ) {
-        return "The Lottery Contract is OnLine";
+      return "The Lottery Contract is OnLine";
     }
 
 }
